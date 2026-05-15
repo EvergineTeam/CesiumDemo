37f8a8d88feafced2b73dbada8f0556860116f7e
Alejandro Juan Pérez <tuketet@gmail.com>
2026-05-15 20:12:13 +0200

    pins colors, name editting, etc

--- CesiumDemo/MyScene.cs
+++ CesiumDemo/MyScene.cs
@@ -5,6 +5,7 @@ using Evergine.Cesium.Components;
 using Evergine.Cesium.Utils;
 using Evergine.Common.Graphics;
 using Evergine.Common.Input.Mouse;
+using Evergine.Components.Graphics3D;
 using Evergine.Framework;
 using Evergine.Framework.Assets.AssetParts;
 using Evergine.Framework.Graphics;
@@ -15,10 +16,14 @@ using Evergine.Framework.Services;
 using Evergine.Mathematics;
 using Evergine.Runtimes.GLB;
 using Evergine.UI;
+using Newtonsoft.Json.Linq;
+using SharpYaml.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using static Azure.Core.HttpHeader;
+using static CesiumDemo.EvergineContent;
 using static Evergine.Bindings.Draco.Draco;
 
 namespace CesiumDemo
@@ -28,15 +33,21 @@ namespace CesiumDemo
         private static readonly string saveAccessTokenFileName_cesium = "cesium_access_token.txt";
         private static readonly string saveAccessTokenFileName_azure = "azure_access_token.txt";
         private byte[] accessTokenBuffer = new byte[512];
-        private byte[] inputTextBuffer = new byte[512];
+        private byte[] geocodingTextBuffer = new byte[256];
+        private byte[] pinNameTextBuffer = new byte[256];
 
         private GraphicsPresenter graphicsPresenter;
         private AssetsService assetsService;
+        private RenderLayerDescription opaqueLayer;
         private CesiumCoordinator cesiumCoordinator = null;
         private DepthPicking depthPicking;
         private Model pinModel = null;
+        private Effect pinsEfect = null;
         private List<Entity> placedPins = new List<Entity>();
+        private List<StandardMaterial> placedPinsMaterials = new List<StandardMaterial>();
         private List<GeocodingAutocompleteSuggestion> geocodingSuggestions = new List<GeocodingAutocompleteSuggestion>();
+        private int edittingPinName = -1;
+        private bool needSetKeyboardFocus = false; // true if we just started editting the pin name, so we still need to set the focus on the InputText box
 
         private unsafe bool imguiWantsCaptureMouse() => ImguiNative.igGetIO_Nil()->WantCaptureMouse == 1;
 
@@ -59,36 +70,26 @@ namespace CesiumDemo
         {
             this.LoadSavedAccessToken(saveAccessTokenFileName_cesium);
             this.assetsService = Application.Current.Container.Resolve<AssetsService>();
-            this.pinModel = await GLBRuntime.Instance.Read("Meshes/pin.glb", (Evergine.Framework.Runtimes.MaterialData materialData) =>
-            {
-                return Task.Run(() =>
-                {
-                    var effect = this.assetsService.Load<Effect>(EvergineContent.Effects.StandardEffect);
-                    var decorator = new StandardMaterial(effect)
-                    {
-                        BaseColor = Color.Red,
-                        
-                    };
-                    return decorator.Material;
-                });
-            });
+            this.pinModel = await GLBRuntime.Instance.Read("Meshes/pin.glb");
+            this.pinsEfect = this.assetsService.Load<Effect>(DefaultResourcesIDs.StandardEffectID);
+            this.opaqueLayer = this.assetsService.Load<RenderLayerDescription>(DefaultResourcesIDs.OpaqueRenderLayerID);
         }
 
-        private static unsafe string u8PtrToString(byte* bufferPtr)
+        private static unsafe string bytePtrToString(byte* bufferPtr)
         {
             return System.Runtime.InteropServices.Marshal.PtrToStringUTF8((IntPtr)bufferPtr);
         }
 
-        private static unsafe string u8ArrayToString(byte[] buffer)
+        private static unsafe string byteArrayToString(byte[] buffer)
         {
             fixed (byte* bufferPtr = buffer)
             {
-                return u8PtrToString(bufferPtr);
+                return bytePtrToString(bufferPtr);
             }
         }
 
-        private string getAccessTokenStr() => u8ArrayToString(accessTokenBuffer);
-        private string getInputTextStr() => u8ArrayToString(inputTextBuffer);
+        private string getAccessTokenStr() => byteArrayToString(accessTokenBuffer);
+        private string getInputTextStr() => byteArrayToString(geocodingTextBuffer);
 
         private void LoadSavedAccessToken(string filePath)
         {
@@ -138,13 +139,13 @@ namespace CesiumDemo
         {
             base.Draw(deltaTime);
 
-            var lines = (this.Managers.RenderManager as RenderManager).LineBatch3D;
-            foreach (var pinEntity in this.placedPins)
-            {
-                var transform = pinEntity.FindComponent<Transform3D>();
-                var box = new BoundingOrientedBox(transform.Position, transform.Scale, transform.Orientation);
-                lines.DrawBoundingOrientedBox(box, Color.Red);
-            }
+            //var lines = (this.Managers.RenderManager as RenderManager).LineBatch3D;
+            //foreach (var pinEntity in this.placedPins)
+            //{
+            //    var transform = pinEntity.FindComponent<Transform3D>();
+            //    var box = new BoundingOrientedBox(transform.Position, transform.Scale, transform.Orientation);
+            //    lines.DrawBoundingOrientedBox(box, Color.Red);
+            //}
 
             if (this.cesiumCoordinator == null ||
                 this.cesiumCoordinator.CurrentStatus == Status.Uninitialized ||
@@ -181,7 +182,7 @@ namespace CesiumDemo
 
             ImguiNative.igSetNextWindowPos(new Vector2(screenWidth, screenHeight) * 0.5f, ImGuiCond.Once, new Vector2(0.5f, 0.5f));
             ImguiNative.igSetNextWindowSize(new Vector2(windowWidth, windowHeight), ImGuiCond.Once);
-            ImguiNative.igBegin("Cesium", null, (ImGuiWindowFlags)0);
+            ImguiNative.igBegin("Cesium##start", null, (ImGuiWindowFlags)0);
             fixed (byte* accessTokenBufferPtr = accessTokenBuffer)
             {
                 ImguiNative.igText("This is a demo of Evergine Cesium addon.");
@@ -228,9 +229,9 @@ namespace CesiumDemo
 
         private unsafe void gui_mainMenu()
         {
-            ImguiNative.igSetNextWindowPos(new Vector2(0, 0) * 0.5f, ImGuiCond.Once, new Vector2(0.5f, 0.5f));
-            ImguiNative.igSetNextWindowSize(new Vector2(120, 400), ImGuiCond.Once);
-            ImguiNative.igBegin("Cesium", null, (ImGuiWindowFlags)0);
+            ImguiNative.igSetNextWindowPos(new Vector2(0, 0), ImGuiCond.FirstUseEver, new Vector2(0, 0));
+            ImguiNative.igSetNextWindowSize(new Vector2(290, 500), ImGuiCond.FirstUseEver);
+            ImguiNative.igBegin("Cesium##main", null, (ImGuiWindowFlags)0);
             { // Provider selection
                 var currentProvider = cesiumCoordinator.OverlayProvider;
                 var availableProviders = new[] {
@@ -265,10 +266,10 @@ namespace CesiumDemo
                 ImguiNative.igSeparatorText("Geocoding");
                 if (this.cesiumCoordinator.IsGeocodingConfigured)
                 {
-                    fixed (byte* inputTextBufferPtr = this.inputTextBuffer)
+                    fixed (byte* inputTextBufferPtr = this.geocodingTextBuffer)
                     {
                         if (ImguiNative.igInputTextWithHint("##geocoding_input", "Type city or address",
-                            inputTextBufferPtr, (uint)this.inputTextBuffer.Length,
+                            inputTextBufferPtr, (uint)this.geocodingTextBuffer.Length,
                             ImGuiInputTextFlags.EnterReturnsTrue | ImGuiInputTextFlags.CallbackEdit, (ImGuiInputTextCallbackData * data) =>
                             {
                                 if ((data->EventFlag & ImGuiInputTextFlags.CallbackEdit) != 0)
@@ -280,7 +281,7 @@ namespace CesiumDemo
                                     }
                                     else
                                     {
-                                        this.cesiumCoordinator.AutocompleteAsync(u8PtrToString(data->Buf))
+                                        this.cesiumCoordinator.AutocompleteAsync(bytePtrToString(data->Buf))
                                             .ContinueWith((res) =>
                                             {
                                                 this.geocodingSuggestions.Clear();
@@ -311,7 +312,7 @@ namespace CesiumDemo
                 }
                 else
                 {
-                    ImguiNative.igText("To enable geocoding features, please input an Azure Maps access token:");
+                    ImguiNative.igText("To enable geocoding features,\nplease input an Azure Maps access token:");
                     var textBoxSize = new Vector2(-float.Epsilon, ImguiNative.igGetTextLineHeight() * 10);
                     fixed (byte* accessTokenBufferPtr = accessTokenBuffer)
                     {
@@ -331,23 +332,99 @@ namespace CesiumDemo
                 int deleteInd = -1;
                 for (int i = 0; i < this.placedPins.Count; i++)
                 {
+                    ImguiNative.igPushID_Int(0);
                     var pin = this.placedPins[i];
-                    ImguiNative.igText($"{pin.Name}");
-                    ImguiNative.igSameLine(0, 0);
+                    var pinMaterial = this.placedPinsMaterials[i];
+                    if (ImguiNative.igColorButton($"##color_{i}", pinMaterial.BaseColor.ToVector4(), ImGuiColorEditFlags.NoTooltip, new Vector2(0, 0)))
+                    {
+                        ImguiNative.igOpenPopup_Str($"pin_color_popup_{i}", ImGuiPopupFlags.None);
+                    }
+                    if (ImguiNative.igBeginPopup($"pin_color_popup_{i}", ImGuiWindowFlags.None))
+                    {
+                        var paletteColors = new Vector4[]
+                        {
+                           new Vector4(1, 0, 0, 1),
+                           new Vector4(0, 1, 0, 1),
+                           new Vector4(0, 0, 1, 1),
+                           new Vector4(1, 1, 0, 1),
+                           new Vector4(0, 1, 1, 1),
+                           new Vector4(1, 0, 1, 1),
+                           new Vector4(0, 0, 0, 1),
+                           new Vector4(1, 1, 1, 1),
+                        };
+                        for (int pi = 0; pi < paletteColors.Length; pi++)
+                        {
+                            if (ImguiNative.igColorButton($"##palette_{pi}", paletteColors[pi], ImGuiColorEditFlags.NoTooltip, new Vector2(0, 0)))
+                            {
+                                pinMaterial.BaseColor = Color.FromVector4(ref paletteColors[pi]);
+                                ImguiNative.igCloseCurrentPopup();
+                            }
+                            if (pi % 4 != 3)
+                            {
+                                ImguiNative.igSameLine(0, 1);
+                            }
+                        }
+                        ImguiNative.igEndPopup();
+                        ImguiNative.igPopID();
+
+                    }
+                    ImguiNative.igSameLine(0, 1);
+
+                    if (this.edittingPinName == i)
+                    {
+                        fixed (byte* pinNameTextBufferPtr = this.pinNameTextBuffer)
+                        {
+                            if (this.needSetKeyboardFocus)
+                            {
+                                ImguiNative.igSetKeyboardFocusHere(0);
+                            }
+                            if (ImguiNative.igInputText($"##edit_name_{i}", pinNameTextBufferPtr, (uint)pinNameTextBuffer.Length, ImGuiInputTextFlags.EnterReturnsTrue, null, null))
+                            {
+                                pin.Name = bytePtrToString(pinNameTextBufferPtr);
+                                this.edittingPinName = -1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ImguiNative.igText($"{pin.Name}");
+                        if (ImguiNative.igBeginPopupContextItem($"pin_right_click{i}", ImGuiPopupFlags.None))
+                        {
+                            if (ImguiNative.igSelectable_Bool("Edit Name##{i}", false, ImGuiSelectableFlags.None, new Vector2(0, 0)))
+                            {
+                                this.edittingPinName = i;
+                                this.needSetKeyboardFocus = true;
+                                for (int nameI = 0; nameI < pin.Name.Length && nameI < this.pinNameTextBuffer.Length; nameI++)
+                                {
+                                    this.pinNameTextBuffer[nameI] = (byte)pin.Name[nameI];
+                                }
+                                this.pinNameTextBuffer[pin.Name.Length] = 0;
+                            }
+                            ImguiNative.igPushStyleColor_Vec4(ImGuiCol.Text, new Vector4(1, 0, 0, 1));
+                            if (ImguiNative.igSelectable_Bool("Delete Pin##{i}", false, ImGuiSelectableFlags.None, new Vector2(0, 0)))
+                            {
+                                deleteInd = i;
+                            }
+                            ImguiNative.igPopStyleColor(1);
+                            ImguiNative.igEndPopup();
+                        }
+                    }
+
+                    ImguiNative.igSameLine(0, 1);
                     if (ImguiNative.igButton($"GO##{i}", new Vector2(0, 0)))
                     {
                         var placeComp = pin.FindComponent<CesiumPlacerComponent>();
                         this.cesiumCoordinator.WorldCamera.FlyTo(MathHelper.ToDegrees(placeComp.Latitude), MathHelper.ToDegrees(placeComp.Longitude), 2.0);
                     }
-                    ImguiNative.igSameLine(0, 0);
-                    ImguiNative.igPushStyleColor_Vec4(ImGuiCol.Button, new Vector4(1, 0, 0, 1));
-                    ImguiNative.igPushStyleColor_Vec4(ImGuiCol.ButtonHovered, new Vector4(1, 0.3f, 0.3f, 1));
-                    ImguiNative.igPushStyleColor_Vec4(ImGuiCol.ButtonActive, new Vector4(1, 0.5f, 0.5f, 1));
-                    if (ImguiNative.igButton($"delete##{i}", new Vector2(0, 0)))
-                    {
-                        deleteInd = i;
-                    }
-                    ImguiNative.igPopStyleColor(3);
+                    //ImguiNative.igSameLine(0, 1);
+                    //ImguiNative.igPushStyleColor_Vec4(ImGuiCol.Button, new Vector4(1, 0, 0, 1));
+                    //ImguiNative.igPushStyleColor_Vec4(ImGuiCol.ButtonHovered, new Vector4(1, 0.3f, 0.3f, 1));
+                    //ImguiNative.igPushStyleColor_Vec4(ImGuiCol.ButtonActive, new Vector4(1, 0.5f, 0.5f, 1));
+                    //if (ImguiNative.igButton($"delete##{i}", new Vector2(0, 0)))
+                    //{
+                    //    deleteInd = i;
+                    //}
+                    //ImguiNative.igPopStyleColor(3);
                 }
                 if (deleteInd >= 0 && deleteInd < this.placedPins.Count)
                 {
@@ -355,6 +432,7 @@ namespace CesiumDemo
                     pin.Parent.DetachChild(pin);
                     pin.Destroy();
                     this.placedPins.RemoveRange(deleteInd, 1);
+                    this.placedPinsMaterials.RemoveRange(deleteInd, 1);
                 }
             }
             ImguiNative.igEnd();
@@ -390,13 +468,23 @@ namespace CesiumDemo
                     Longitude = cartographic.Longitude,
                     Height = cartographic.Height,
                     AboveTerrain = false,
-                    Rotation = Quaternion.Identity,
+                    Rotation = Quaternion.CreateFromYawPitchRoll(0.4f, -0.9f, 0),
                 };
                 var pinEntity = this.pinModel.InstantiateModelHierarchy(this.assetsService);
                 pinEntity.Name = $"Pin {this.placedPins.Count}";
                 pinEntity.AddComponent(placerComp);
                 pinEntity.AddComponent(new ConstantSizeBillboard() { Scale = 0.01f });
+                var materialComp = pinEntity.FindComponentInChildren<MaterialComponent>();
+                var material = new StandardMaterial(this.pinsEfect)
+                {
+                    BaseColor = new Color(255, 0, 0, 255),
+                    Metallic = 0,
+                    Roughness = 1,
+                    LayerDescription = opaqueLayer,
+                };
+                materialComp.Material = material.Material;
                 this.placedPins.Add(pinEntity);
+                this.placedPinsMaterials.Add(material);
                 this.cesiumCoordinator.Root.AddChild(pinEntity);
             }
         }
